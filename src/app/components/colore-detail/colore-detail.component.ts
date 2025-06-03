247dcc4ff4576e1ef387a1d9d529ae72930b8e2c
johnjybn <johnjulongbayan04@gmail.com>
2025-06-03 22:43:44 +0200

    finale

--- src/app/components/colore-detail/colore-detail.component.ts
+++ src/app/components/colore-detail/colore-detail.component.ts
@@ -1,21 +1,18 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Colore } from '../../models/colore';
-import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-colore-detail',
-  imports: [NgClass],
+  imports: [CommonModule],
   templateUrl: './colore-detail.component.html',
   styleUrl: './colore-detail.component.css'
 })
 export class ColoreDetailComponent {
-  @Input()
-  colore?: Colore;
+  @Input() colore!: Colore;
+  @Output() cliccato = new EventEmitter<void>();
 
-  @Output()
-  cliccato = new EventEmitter<void>();
-
-  onClick() {
+  onClicked() {
     this.cliccato.emit();
   }
 }
